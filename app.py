203ecc77aea06ef6cc10fc2b4a10ab7dd5fcdcc7
fatemeh pourmand <fatemeepourmand@gmail.com>
2025-11-25 10:15:37 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,33 @@
+Class user:
+   def__init__(self,name):
+      self.name= name
+      self.chat_history = []
 
+   def send_message(self, message):
+       self.chat_history.append(message)
+       
+Class patient(user):
+    def __init__(self,name)
+    suoer().__init__(name)
+    self.emptiom_history = []
+
+ def log_emotion(self, emotion):
+       self.emotion_history.appened(emotion)
+       
+Class Psychologist(user):
+    def __init__(slef, name):
+        super().__init__(name)
+        self.patients = []
+    
+    def add_patient(slef, patient):
+        self.patients.append(patient)
+        
+    def view_alert(slef):
+        
+        for p in seld.patients:
+            print(f"{p.name} has {len(p.emotion_history)} emotion rntries")
+
+Class DailyEmotion
+
+  def
+pass..
